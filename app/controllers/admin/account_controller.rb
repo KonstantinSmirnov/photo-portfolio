49a7168761735d4e333565333627d6c83461d5e3
konstantinsmirnov <k.smi@mail.ru>
2017-04-22 12:56:54 +0300

    Add update password

--- app/controllers/admin/account_controller.rb
+++ app/controllers/admin/account_controller.rb
@@ -4,13 +4,31 @@ class Admin::AccountController < AdminController
   end
 
   def update
+
     if current_user.valid_password?(params[:user][:password])
-      current_user.update_attributes(:email => params[:user][:email])
-      flash[:success] = 'Email has been updated'
-      redirect_to admin_account_path
+      case params[:target]
+      when 'email'
+        current_user.update_attributes(:email => params[:user][:email])
+        flash[:success] = 'Email has been updated'
+        redirect_to admin_account_path
+      when 'password'
+        current_user.password = params[:user][:new_password]
+        current_user.password_confirmation = params[:user][:new_password_confirmation]
+        if current_user.valid? && !params[:user][:new_password].blank?
+          current_user.save!
+          flash[:success] = 'Password has been updated'
+          redirect_to admin_account_path
+        else
+          flash.now[:danger] = 'New password is invalid'
+          render :index
+        end
+      end
+
     else
       flash.now[:danger] = 'Invalid password'
       render :index
     end
+
+
   end
 end
--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -9,38 +9,81 @@ feature 'ACCOUNT' do
   end
 
   context 'user tries to update email' do
-    scenario 'fails if password is empty' do
+    scenario 'it fails if password is empty' do
       fill_in 'update_email_email', with: 'another@email.com'
       fill_in 'update_email_password', with: ''
-      click_button 'Update'
+      click_button 'Update email'
 
       expect(page).to have_selector('.flash-alert.flash-danger', text: 'Invalid password')
     end
 
-    scenario 'fails if password is invalid' do
+    scenario 'it fails if password is invalid' do
       fill_in 'update_email_email', with: 'another@email.com'
       fill_in 'update_email_password', with: 'invalid_password'
-      click_button 'Update'
+      click_button 'Update email'
 
       expect(page).to have_selector('.flash-alert.flash-danger', text: 'Invalid password')
     end
 
-    scenario 'not updated if email is empty' do
+    scenario 'is not updated if email is empty' do
       fill_in 'update_email_email', with: ''
       fill_in 'update_email_password', with: 'password'
-      click_button 'Update'
+      click_button 'Update email'
 
       expect(find_field('update_email_email').value).to eq(admin.email)
     end
 
-    scenario 'updates with valid password' do
+    scenario 'it updates with valid password' do
       new_email = 'new@email.com'
       fill_in 'update_email_email', with: new_email
       fill_in 'update_email_password', with: 'password'
-      click_button 'Update'
+      click_button 'Update email'
 
       expect(page).to have_selector('.flash-alert.flash-success', text: 'Email has been updated')
       expect(find_field('update_email_email').value).to eq(new_email)
+    end
+  end
+
+  context 'user tries to update password' do
+    scenario 'it fails if password is empty' do
+      fill_in 'update_password_password', with: ''
+      click_button 'Update password'
+
+      expect(page).to have_selector('.flash-alert.flash-danger', text: 'Invalid password')
+    end
+
+    scenario 'it fails if new password is empty' do
+      fill_in 'update_password_password', with: 'password'
+      click_button 'Update password'
+
+      expect(page).to have_selector('.flash-alert.flash-danger', text: 'New password is invalid')
+    end
+
+    scenario 'it fails if new password is short' do
+      fill_in 'update_password_password', with: 'password'
+      fill_in 'update_password_new_password', with: '12'
+      fill_in 'update_password_new_password_confirmation', with: '12'
+      click_button 'Update password'
+
+      expect(page).to have_selector('.flash-alert.flash-danger', text: 'New password is invalid')
+    end
+
+    scenario 'it fails if new password and confirmation do not match' do
+      fill_in 'update_password_password', with: 'password'
+      fill_in 'update_password_new_password', with: '123123'
+      fill_in 'update_password_new_password_confirmation', with: '123'
+      click_button 'Update password'
+
+      expect(page).to have_selector('.flash-alert.flash-danger', text: 'New password is invalid')
+    end
+
+    scenario 'id succeed with valid data' do
+      fill_in 'update_password_password', with: 'password'
+      fill_in 'update_password_new_password', with: '123123'
+      fill_in 'update_password_new_password_confirmation', with: '123123'
+      click_button 'Update password'
+
+      expect(page).to have_selector('.flash-alert.flash-success', text: 'Password has been updated')
     end
   end
 end
