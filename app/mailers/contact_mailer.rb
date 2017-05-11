33af0740f76ad725cff7af7ce331e1282f23d4ba
konstantinsmirnov <k.smi@mail.ru>
2017-05-11 20:38:55 +0300

    Add page title in email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,8 +2,8 @@ class ContactMailer < ApplicationMailer
   default from: "photo@smiplay.com"
 
   def new_contact_email(email)
-    title = Setting.first.webpage_title
-    page_title = title ? title : 'photopage'
+    setting = Setting.first
+    page_title = (setting.webpage_title if setting) || 'photopage'
     @email = email
     mail to: User.first.email, subject: "Letter from " + page_title
   end
