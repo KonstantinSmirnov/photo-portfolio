35faadd35be44bbbe4c2e2504939fd09e3172638
KonstantinSmirnov <k.smi@mail.ru>
2017-07-13 00:47:14 +0300

    Update prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
+  config.log_level = :error # this will the rails to log error only in production
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
